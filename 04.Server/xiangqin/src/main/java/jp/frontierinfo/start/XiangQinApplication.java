91f737bae37a3a9ba68005895bf7fc7ada100b6f
wuzhusong <815968847@qq.com>
2022-07-05 13:57:51 +0900

    吴 -- 调整环境配置文件

--- 04.Server/xiangqin/src/main/java/jp/frontierinfo/start/XiangQinApplication.java
+++ 04.Server/xiangqin/src/main/java/jp/frontierinfo/start/XiangQinApplication.java
@@ -1,9 +1,13 @@
-package jp.frontierinfo.api;
+package jp.frontierinfo.start;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("jp.frontierinfo")
+@MapperScan("jp.frontierinfo.db")
 public class XiangQinApplication {
 
     public static void main(String[] args) {
