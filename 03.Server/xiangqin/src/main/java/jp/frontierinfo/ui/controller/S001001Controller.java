5965e8545de843a14a3b7d735ca46584eaf4227a
wuzhusong <815968847@qq.com>
2022-09-05 21:22:25 +0900

    吴竹松 Web 添加S001001Controller

---
+++ 03.Server/xiangqin/src/main/java/jp/frontierinfo/ui/controller/S001001Controller.java
@@ -0,0 +1,44 @@
+package jp.frontierinfo.ui.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.frontierinfo.ui.form.S001001Form;
+import jp.frontierinfo.ui.input.S001001E000Input;
+
+@Controller
+@RequestMapping("/ui")  
+public class S001001Controller {
+	
+
+	@RequestMapping(value="/s001001", method=RequestMethod.GET)
+	public String e000(HttpServletRequest request, HttpServletResponse response, 
+			S001001Form form, S001001E000Input input, Model model) {
+		request.getSession();
+		request.getParameter("username");
+		return "view/s001001";
+	}
+	@RequestMapping(value="/s001001", params="login", method=RequestMethod.POST)
+	public String e001(HttpServletRequest request, HttpServletResponse response, 
+			S001001Form form, S001001E000Input input) {
+		
+		return "view/s001001";
+	}
+	@RequestMapping(value="/s001001", method=RequestMethod.POST)
+	public String e002(HttpServletRequest request, HttpServletResponse response, 
+			S001001Form form, S001001E000Input input) {
+		
+		return "view/s001001";
+	}
+	@RequestMapping(value="/s001001", method=RequestMethod.POST)
+	public String e003(HttpServletRequest request, HttpServletResponse response, 
+			S001001Form form, S001001E000Input input) {
+		
+		return "view/s001001";
+	}
+}
