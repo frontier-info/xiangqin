ca49c3ebb0421283055d1a43aa53cf707ab9a2f5
wuzhusong <815968847@qq.com>
2022-11-13 17:28:24 +0900

    吴竹松 Web 调整返回值

--- 03.Server/xiangqin/src/main/java/jp/frontierinfo/db/bean/userSimpleInfo.java
+++ 03.Server/xiangqin/src/main/java/jp/frontierinfo/db/bean/userSimpleInfo.java
@@ -5,8 +5,12 @@ import lombok.Data;
 @Data
 public class userSimpleInfo {
 	
-	private String uid;
-	
 	private String uname;
+	
+	private Integer sex;
+	
+	private String introduce;
+	
+	private String uimages;
 
 }
