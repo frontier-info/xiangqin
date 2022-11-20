05d8e927c3421067a55131892a02c10017c72c5b
liudaguang <liudaguang43@gmail.com>
2022-11-20 16:59:23 +0900

    刘大光002001controller提交

--- 03.Server/xiangqin/src/main/java/jp/frontierinfo/ui/controller/S002001Controller.java
+++ 03.Server/xiangqin/src/main/java/jp/frontierinfo/ui/controller/S002001Controller.java
@@ -12,17 +12,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import jp.frontierinfo.common.annotation.PrintLog;
+import jp.frontierinfo.common.constant.ConstantInfo;
 import jp.frontierinfo.common.exception.BusinessException;
+import jp.frontierinfo.db.entity.T01UserLoginInfo;
 import jp.frontierinfo.ui.form.S001001Form;
+import jp.frontierinfo.ui.form.S002001Form;
 import jp.frontierinfo.ui.form.S002002Form;
 import jp.frontierinfo.ui.form.S002003Form;
 import jp.frontierinfo.ui.input.S001001E001Input;
 import jp.frontierinfo.ui.input.S002001E001Input;
 import jp.frontierinfo.ui.input.S002001E002Input;
+import jp.frontierinfo.ui.input.S002001Input;
 import jp.frontierinfo.ui.output.S002001E001Output;
 import jp.frontierinfo.ui.output.S002001E002Output;
+import jp.frontierinfo.ui.output.S002001Output;
 import jp.frontierinfo.ui.service.S002001E001Service;
 import jp.frontierinfo.ui.service.S002001E002Service;
+import jp.frontierinfo.ui.service.S002001Service;
 
 @Controller
 @RequestMapping("/ui")  
@@ -30,6 +36,9 @@ public class S002001Controller {
 	
 	@Autowired
 	private S002001E001Service s002001E001Service;
+	
+	@Autowired
+	private S002001Service s002001Service;
 	
 	@Autowired
 	private S002001E002Service s002001E002Service;
@@ -50,9 +59,25 @@ public class S002001Controller {
 	@PrintLog("登录后跳转用户主页")
 	@RequestMapping(value="/s002001/e000", method=RequestMethod.POST)
 	public String e000(HttpServletRequest request, HttpServletResponse response, 
-			S002002Form form, BindingResult result, 
-			S002001E001Input input, Model model) {
+			S002001Form form, BindingResult result, 
+			S002001Input input, Model model) {
 
+		//用户信息备选人员信息跳转
+        T01UserLoginInfo userLoginInfo = (T01UserLoginInfo) request.getSession().getAttribute(ConstantInfo.USER_LOGIN_INFO);
+        input.setUid(userLoginInfo.getUid());
+        
+		S002001Output output = new S002001Output();
+		try {
+			output = s002001Service.execute(input);
+		} catch (BusinessException e) {
+			model.addAttribute("message", e.getMessage());
+        	return "s001001";
+		}
+
+		BeanUtils.copyProperties(output, form);
+		model.addAttribute("s002001Form", form);
+	
+		
 	return "s002001";
 	}
 	/**
